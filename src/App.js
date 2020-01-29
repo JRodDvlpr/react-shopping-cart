1ae926f4668581e5595dc04eba23c88073ff6e28
JRodDvlpr <Mialienproductions@gmail.com>
2020-01-29 13:04:45 -0500

    Step 4-6 Done

--- src/App.js
+++ src/App.js
@@ -7,6 +7,9 @@ import Navigation from './components/Navigation';
 import Products from './components/Products';
 import ShoppingCart from './components/ShoppingCart';
 
+// Context 
+import {ProductContext } from './contexts/ProductContext';
+
 function App() {
 
 	// keeps track of all avail products
@@ -25,24 +28,17 @@ function App() {
 
 	return (
 		<div className="App">
+
+			{/* product context wraps all my child components inside and passes a prop value*/}
+			<ProductContext.Provider value={{ products, addItem }}>
+
 			<Navigation cart={cart} />
 
 			{/* Routes */}
-			<Route
-				exact
-				path="/"
-				render={() => (
-					<Products
-						products={products}
-						addItem={addItem}
-					/>
-				)}
-			/>
+			<Route exact path="/" component={Products} />
+			<Route path="/cart" render={() => <ShoppingCart cart={cart} />} />
 
-			<Route
-				path="/cart"
-				render={() => <ShoppingCart cart={cart} />}
-			/>
+			</ProductContext.Provider>
 		</div>
 	);
 }
